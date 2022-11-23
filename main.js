e4ba39c5867be93f01c47afc143be932f3b6cdb4
RominaMalimban <romina.lyn@hotmail.it>
2022-11-23 16:23:39 +0100

    tentativo realizzazione bottone like

--- main.js
+++ main.js
@@ -83,13 +83,13 @@ posts.forEach((element) =>{
                     <div class="post__footer">
                         <div class="likes js-likes">
                             <div class="likes__cta">
-                                <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
                                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                                     <span class="like-button__label">Mi Piace</span>
                                 </a>
                             </div>
                             <div class="likes__counter">
-                                Piace a <b id=${element.id} class="js-likes-counter">${element.likes}</b> persone
+                                Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
                             </div>
                         </div> 
                     </div>            
@@ -104,6 +104,12 @@ posts.forEach((element) =>{
 // Milestone 3 -
 // Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 
+
+document.querySelector(".like-button").addEventLister("click",
+    function(){
+        document.querySelector(".like-button").className = "like-button--liked"
+    }
+);
 
 
 
